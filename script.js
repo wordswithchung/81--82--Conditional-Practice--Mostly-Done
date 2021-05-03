b58d89fc446974c9cbd4fefdbc1b5310ba27b923
Chung Nguyen <chung.nguyen@gmail.com>
2021-05-03 16:17:10 +0100

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,37 @@
+// potential user names that work: 
+// "Spider Man", "Iron Man", "Black Widow", "Thor", "Black Panther"
+
+// WHAT I WANT TO ACHIEVE:
+// Correct username? 
+// -- Yes: show them the password field
+// -- No: show them an alert 
+// Correct password?
+// -- Yes: show them a welcome message
+// -- No: show them an alert
+
+let correctPassword = "Open Sesame";
+$(".password").hide();
+$(".welcome-message").hide();
+
+$(".button").click(function() {
+    let userName = $(".username").val();
+
+    if (userName === "Spider Man") {
+        $(".password").show();
+    } else if (userName === "Thor") {
+        $(".password").show();
+    } else {
+        alert("Not a valid Marvel hero; get out of here!");
+    }
+
+    let password = $(".password").val();
+    if (password === correctPassword) {
+        $("img").css("width", "500px");
+        $("img").attr("src", "https://wallpaperaccess.com/full/1312528.jpg");
+        $(".username").hide();
+        $(".password").hide();
+        $(".button").hide();
+        $(".welcome-message").text("Welcome back, " + userName + "!");
+        $(".welcome-message").show();
+    }
+});
